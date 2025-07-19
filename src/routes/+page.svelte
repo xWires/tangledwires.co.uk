a9afe18f19a50a7376ac0f98bac8833e2fe12c56
xWires <xwires@tangledwires.xyz>
2025-07-19 18:50:53 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="flex flex-col justify-center items-center p-10" style="padding-top: 50px">
+    <picture class="flex justify-center items-center" style="padding-bottom: 35px">
+        <source srcset="/logo_light2.png" media="(prefers-color-scheme: light)">
+        <source srcset="/logo_dark2.png" media="(prefers-color-scheme: dark)">
+
+        <img src="/logo_light2.png" alt="The word 'TangledWires' next to a computer terminal" width="25%">
+    </picture>
+    <p class="prose text-l">This domain is used to host some TangledWires services.</p>
+    <p class="prose text-l">Looking for the main TangledWires website? Go to <a href="https://tangledwires.xyz/">https://tangledwires.xyz/</a> instead.</p>
+</div>
