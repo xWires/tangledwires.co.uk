183533276081483d430a3db40b9beed1968d1dcc
xWires <xwires@tangledwires.xyz>
2025-07-20 14:02:16 +0100

    Fix logo sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
         <source srcset="/logo_light2.png" media="(prefers-color-scheme: light)">
         <source srcset="/logo_dark2.png" media="(prefers-color-scheme: dark)">
 
-        <img src="/logo_light2.png" alt="The word 'TangledWires' next to a computer terminal" width="25%">
+        <img src="/logo_light2.png" alt="The word 'TangledWires' next to a computer terminal" width="350">
     </picture>
     <p class="prose text-l">This domain is used to host some TangledWires services.</p>
     <p class="prose text-l">Looking for the main TangledWires website? Go to <a href="https://tangledwires.xyz/">https://tangledwires.xyz/</a> instead.</p>
