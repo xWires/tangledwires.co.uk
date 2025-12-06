c29248d7d572a12120d296cd6ada56ef7ce875ea
xWires <xwires@tangledwires.xyz>
2025-12-06 13:53:39 +0000

    Add some stuff to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,18 @@
 </script>
 
 <div class="flex flex-col justify-center items-center p-10" style="padding-top: 50px">
-    <picture class="flex justify-center items-center" style="padding-bottom: 35px">
-        <source srcset="/logo_light2.png" media="(prefers-color-scheme: light)">
-        <source srcset="/logo_dark2.png" media="(prefers-color-scheme: dark)">
+  <picture class="flex justify-center items-center" style="padding-bottom: 35px">
+    <source srcset="/logo_light2.png" media="(prefers-color-scheme: light)">
+    <source srcset="/logo_dark2.png" media="(prefers-color-scheme: dark)">
 
-        <img src="/logo_light2.png" alt="The word 'TangledWires' next to a computer terminal" width="350">
-    </picture>
-    <p class="prose text-l">Sorry, there's nothing here yet.</p>
-    <p class="prose text-l">Come back later.</p>
+    <img src="/logo_light2.png" alt="The word 'TangledWires' next to a computer terminal" width="350">
+  </picture>
+  <div class="flex flex-col justify-center items-center p-10">
+    <p class="text-3xl font-medium">Welcome</p>
+    <p>TangledWires is a software team creating... anything we find interesting really.</p>
+  </div>
+  <div class="flex flex-col justify-center items-center p-10">
+    <p class="text-3xl font-medium">What are we making at the moment?</p>
+    <p>Currently, we're working on Stationary, a mobile app for tracking which UK train stations you've been to.</p>
+  </div>
 </div>
