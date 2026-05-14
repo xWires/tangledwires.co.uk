324c8f4e72dcd3e00d3a5ab7618b96fe08469150
xWires <xwires@tangledwires.xyz>
2026-05-14 13:24:11 +0100

    Correct Stationary text on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col justify-center items-center p-10">
         <p className="text-3xl font-medium">What are we making at the moment?</p>
-        <p>Currently, we&apos;re working on <Link href="/projects/stationary">Stationary</Link>, a mobile app for tracking which UK train stations you&apos;ve been to.</p>
+        <p>Currently, we&apos;re working on <Link href="/projects/stationary">Stationary</Link>, a mobile app for tracking which GB train stations you&apos;ve been to.</p>
       </div>
       <div className="flex flex-row justify-center items-center text-3xl">
         <a href="https://www.facebook.com/tangledwiresofficial/" aria-label="Facebook"><FontAwesomeIcon icon={faFacebook} /></a>
